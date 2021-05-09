6923d855f3e7f4e94de71cd07ef989eb74015d51
WafflesMcDuff <goldjd@gmail.com>
2021-05-09 17:09:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,7 @@
     CURLOPT_CUSTOMREQUEST => 'GET',
     CURLOPT_HTTPHEADER => array(
         'get_logged_in: ',
-        'token: am9zaHNwbGF5Z3JvdW5k',
-        'Cookie: 5d69be776f972f618357ed7009ea7ccb=rdat517c9lalns11r6p36s16ud; __cfduid=d1c77a2fe7002926d77e24e8b8d0f53d51620571489'
+        'token: am9zaHNwbGF5Z3JvdW5k'
     ),
     ));
 
